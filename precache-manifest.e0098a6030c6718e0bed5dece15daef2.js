7f6e5c787e23357e8a15024de8ecc55c2aff69ea
Adrian <adrberia@gmail.com>
2020-05-27 21:53:06 -0400

    Updates

---
+++ precache-manifest.e0098a6030c6718e0bed5dece15daef2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd3f5e26555b6a2a763853428b8accde",
+    "url": "/React-Bitcoin-Chart/index.html"
+  },
+  {
+    "revision": "6a5b5d7ea62986310c6e",
+    "url": "/React-Bitcoin-Chart/static/css/main.462b0bd9.chunk.css"
+  },
+  {
+    "revision": "247bfaebb9718c49bc2d",
+    "url": "/React-Bitcoin-Chart/static/js/2.1e483d28.chunk.js"
+  },
+  {
+    "revision": "583ce86d7f120f4227d5f98b4c47211e",
+    "url": "/React-Bitcoin-Chart/static/js/2.1e483d28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a5b5d7ea62986310c6e",
+    "url": "/React-Bitcoin-Chart/static/js/main.310bf896.chunk.js"
+  },
+  {
+    "revision": "efca76530cb7bcd9c03b",
+    "url": "/React-Bitcoin-Chart/static/js/runtime-main.f08a087e.js"
+  }
+]);
